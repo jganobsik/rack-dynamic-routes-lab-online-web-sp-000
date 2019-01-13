a6795ee0755baf71c9b86e00577cf64ce974af65
Joe Ganobsik <jganobsik@gmail.com>
2019-01-13 15:15:17 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+   def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+  
+   if req.path.match(/items/)
+ 
+      item_title = req.path.split("/items/").last 
+      item = @@items.find{|s| s.name == item_title}
+ 
+      resp.write item.price
+    end
+ 
+  
+    resp.finish
+  end
+  
+end
